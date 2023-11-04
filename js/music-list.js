be328163f0dd1d397e11fb1041a79be8e3ac89c2
Suman Mondal <mondalsuman97322@gmail.com>
2023-11-04 10:47:48 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,128 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "On My Way",
+    artist: "Allan Walker",
+    img: "1",
+    src: "1"
+  },
+  {
+    name: "FADED",
+    artist: "Allan Walker",
+    img: "2",
+    src: "2"
+  },
+  {
+    name: "Cartoon Mas-Y-Mas",
+    artist: "NCS Release",
+    img: "3",
+    src: "3"
+  },
+  {
+    name: "WaRRikko",
+    artist: "ELPORT Remix",
+    img: "4",
+    src: "4"
+  },
+  {
+    name: "NCS Music",
+    artist: "NCS Release",
+    img: "6",
+    src: "6"
+  },
+  {
+    name: "TAMASHA",
+    artist: "T-Series Release",
+    img: "7",
+    src: "7"
+  },
+  {
+    name: "SANAK",
+    artist: "Zee Music",
+    img: "8",
+    src: "8"
+  },
+  {
+    name: "DILBAR",
+    artist: "T-Series",
+    img: "9",
+    src: "9"
+  },
+  {
+    name: "Luka-Chupi",
+    artist: "T-Series",
+    img: "10",
+    src: "10"
+  },
+  {
+    name: "Lagdi-Lahore-Di",
+    artist: "T-Series",
+    img: "11",
+    src: "11"
+  },
+  {
+    name: "Putt Jat Da",
+    artist: "GANNA Exclusive",
+    img: "12",
+    src: "12"
+  },
+  {
+    name: "Atif-Islam-Music",
+    artist: "",
+    img: "13",
+    src: "13"
+  },
+  {
+    name: "Vaste",
+    artist: "T-Series",
+    img: "14",
+    src: "14"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
